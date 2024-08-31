995fcecca4300216c5772f44210d33e57863f08e
smallDazza <smalldazza@gmail.com>
2024-08-31 12:10:47 +1000

    added validation

--- models/card.py
+++ models/card.py
@@ -1,5 +1,10 @@
 from init import db, ma
-from marshmallow import fields
+from marshmallow import fields, validates
+from marshmallow.validate import Length, And, Regexp, OneOf
+from marshmallow.exceptions import ValidationError
+
+VALID_STATUSES = ("To Do", "In Progress", "Completed", "Testing", "Deployed") 
+VALID_PRIORITIES = ("Low", "Medium", "High")
 
 class Card(db.Model):
     __tablename__ = "cards"
@@ -20,8 +25,27 @@ class CardSchema(ma.Schema):
     user = fields.Nested("UserSchema", only=["id", "name", "email"])
     comments = fields.List(fields.Nested('CommentSchema', exclude=['card']))
 
+    title = fields.String(required=True, validate=And(Length(min=4, error="Title must be at least 4 characters in length."), Regexp("^[A-Z][A-Za-z0-9 ]+$", error="Title must start with a capital letter & have alphanumeric characters only")))
+
+    status = fields.String(validate=OneOf(VALID_STATUSES))
+    priority = fields.String(validate=OneOf(VALID_PRIORITIES, error="Invalid priority entered"))
+
+    @validates("status")
+    def validate_status(self, value):
+        # if trying to see the value of status as "In Progress"
+        if value == VALID_STATUSES[1]:
+            # check whether an existing In Progress card exists or not
+            # select count(*) from table_name where status="In Progress"
+            stmt = db.select(db.func.count()).select_from(Card).filter_by(status=VALID_STATUSES[1])
+            count = db.session.scalar(stmt)
+            # if it exists
+            if count > 0:
+                raise ValidationError("You already have an In Progress card.")
+                # send error message
+
     class Meta:
         fields = ("id", "title", "description", "status", "priority", "date", "user", "comments")
+        ordered = True
 
 card_schema = CardSchema()
 cards_schema = CardSchema(many=True)
